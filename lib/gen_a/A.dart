280a6ba76c293a9d5beea60d12272f058db48c5c
nothingknowaboutme12 <nothingknowaboutme12@gmail.com>
2023-02-08 21:50:35 +0500

    change2

---
+++ lib/gen_a/A.dart
@@ -0,0 +1,12 @@
+class A{
+ //auto gen ,do not edit! 
+static  final String assets_logo="assets/logo.png";
+static  final String assets_CardSwiperAsset_img2="assets/CardSwiperAsset/img2.jpg";
+static  final String assets_CardSwiperAsset_img3="assets/CardSwiperAsset/img3.jpg";
+static  final String assets_CardSwiperAsset_img1="assets/CardSwiperAsset/img1.jpg";
+static  final String assets_icon_logo="assets/icon_logo.jpeg";
+static  final String assets_test="assets/test.jpeg";
+static  final String assets_ic_launcher="assets/ic_launcher.png";
+static  final String assets_budgetGraph="assets/budgetGraph.png";
+static  final String assets_graph="assets/graph.png";
+}
